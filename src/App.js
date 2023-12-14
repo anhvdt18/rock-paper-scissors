275b6d73331d1de87e0e29576b5a704fbe402c1d
Van Duc Tuan Anh <strategy008@vans-air.0.50.168.192.in-addr.arpa>
2023-12-14 20:42:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [play, setPlay] = useState("LET PLAY");
+
+  let playerHand;
+  let npcHand;
+
+  const match = () => {
+    let a = playerHand + npcHand;
+    console.log(a, "a is here");
+    if (a === "11" || a === "22" || a === "33") {
+      setPlay("PEACE");
+    } else if (a === "13" || a === "21" || a === "32") {
+      setPlay("YOU WIN");
+    } else {
+      setPlay("YOU LOST");
+    }
+    return;
+  };
+
+  let color =
+    play === "YOU WIN" ? "green" : play === "YOU LOST" ? "red" : "blue";
+
+  const rock = () => {
+    npcHand = npcPlay();
+    playerHand = "1";
+    match();
+    console.log(playerHand + npcHand, "ROCK");
+  };
+  const paper = () => {
+    npcHand = npcPlay();
+    playerHand = "2";
+    match();
+    console.log(playerHand + npcHand, "PAPER");
+  };
+  const cissors = () => {
+    npcHand = npcPlay();
+    playerHand = "3";
+    match();
+    console.log(playerHand + npcHand, "CISSORS");
+  };
+
+  const npcPlay = (min = 1, max = 3) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <label className="label">Result</label>
+      <span style={{ color }} className="result">
+        {play}
+      </span>
+      <div className="team-play">
+        <div className="player-icon">👤</div>
+        <label className="label">VS</label>
+        <div className="player-icon">🖥</div>
+      </div>
+      <div className="choice-group">
+        <span onClick={rock} className="hand-choice">
+          👊
+        </span>
+        <span onClick={paper} className="hand-choice">
+          🖐
+        </span>
+        <span onClick={cissors} className="hand-choice">
+          ✌
+        </span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
